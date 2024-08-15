ab609caa4bd882d04f74a4ac2d7b9985e484dd2e
MarceloSmg <celomsg@hotmail.com>
2024-08-15 20:49:32 -0300

    Corrigindo erro de Streamlit ao definir page_config

--- c.py
+++ c.py
@@ -12,22 +12,22 @@ st.set_page_config(page_title="Instituto Tarumã Pesquisa", page_icon="🌲")
 
 # Injetando o CSS para esconder o ícone do GitHub
 
-hide_github_icon = """
-    <style>
-    .viewerBadge_container__1QSob { 
-        display: none; 
-    }
-    .main > div:first-of-type > div > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div > div > a:nth-of-type(1) { 
-        display: none; 
-    }
-    .main > div:first-of-type > div > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div > div > a:nth-of-type(2) { 
-        display: none; 
-    }
-    </style>
-    """
+# hide_github_icon = """
+#     <style>
+#     .viewerBadge_container__1QSob { 
+#         display: none; 
+#     }
+#     .main > div:first-of-type > div > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div > div > a:nth-of-type(1) { 
+#         display: none; 
+#     }
+#     .main > div:first-of-type > div > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div > div > a:nth-of-type(2) { 
+#         display: none; 
+#     }
+#     </style>
+#     """
 
-# Aplicando o estilo ao Streamlit
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+# # Aplicando o estilo ao Streamlit
+# st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Configuração da página
 st.set_page_config(page_title="Instituto Tarumã Pesquisa", page_icon="🌲")
